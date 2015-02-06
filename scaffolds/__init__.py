d47cc1ae3bc88b6404c3634bf9ad4f29a1c4b9b2
Charl Joseph Mert <charl.mert@gmail.com>
2015-02-06 10:00:56 +0200

    Fixed deform assets

--- scaffolds/__init__.py
+++ scaffolds/__init__.py
@@ -6,11 +6,12 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static_deform', 'deform:static')
+    
     config.add_route('form_view', '/')
-
     config.add_route('view', '/view')
-    config.add_static_view('static', 'deform:static')
-
+    
     config.scan()
     return config.make_wsgi_app()
