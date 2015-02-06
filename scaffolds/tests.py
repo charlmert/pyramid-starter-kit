cf76d2ecfc244954b93b1392fa3dc74a367b48dc
Charl Joseph Mert <charl.mert@gmail.com>
2015-02-06 09:06:00 +0200

    Added functional tests

--- scaffolds/tests.py
+++ scaffolds/tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from pyramid import testing
-
+from collections import OrderedDict
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
@@ -14,4 +14,49 @@ class ViewTests(unittest.TestCase):
         from .views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
-        self.assertEqual(info['project'], 'scaffolds')
+        
+        self.assertEqual(info['error_message'], None)
+        self.assertEqual(info['content'], None)
+        self.assertEqual(info['url'], None)
+        self.assertTrue('id="deform"' in info['form'])
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from scaffolds import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_expected_responses(self):
+        # Testing with google.com
+        res = self.testapp.post(
+            '/view', 
+            OrderedDict([
+                ('url', 'www.google.com'),
+                ('submit', 'doPost')
+            ]),
+            status=200)
+        
+        self.assertTrue('www.google.com is not a valid wikipedia url' in res.body)
+
+        # Testing with http://en.wikipedia.org/wiki/Wikipedia
+        res = self.testapp.post(
+            '/view', 
+            OrderedDict([
+                ('url', 'http://en.wikipedia.org/wiki/Wikipedia'),
+                ('submit', 'doPost')
+            ]),
+            status=200)
+
+        self.assertTrue('<p class="lead"><br/>The table of contents for <span class="font-normal"><a href="http://en.wikipedia.org/wiki/Wikipedia">http://en.wikipedia.org/wiki/Wikipedia</a></span></p>' in res.body)
+        
+        # Testing with http://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost
+        res = self.testapp.post(
+            '/view', 
+            OrderedDict([
+                ('url', 'http://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost'),
+                ('submit', 'doPost')
+            ]),
+            status=200)
+
+        self.assertTrue('<p class="lead">The wikipedia page @ <a href="http://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost">http://en.wikipedia.org/wiki/Wikipedia:Wikipedia_Signpost</a> didn\'t contain any identifiable table of contents. Try another URL</p>' in res.body)
